c0a34be376027801ad309358c749eb247787bc97
Thefederico <federico_upegui.dev@outlook.com>
2023-06-26 21:55:42 -0500

    texxt area grid

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,39 @@
 import '@/style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div class="bg-slate-900">
-    <p class="text-gray-200 underline">
-      Holi
-    </p>
-  </div>
-`
+const $ = (selector: string) => document.querySelector(selector)
 
-document.querySelector<HTMLButtonElement>('#counter')!
+const $js = $('#js')
+const $css = $('#css')
+const $html = $('#html')
+
+const update = () => {
+  const html = createHtml()
+
+  $('iframe')?.setAttribute('srcdoc', html)
+}
+
+$js?.addEventListener('input',  update)
+$css?.addEventListener('input', update)
+$html?.addEventListener('input', update)
+
+
+const createHtml = () => {
+  const html = ($html as HTMLInputElement).value
+  const css = ($css as HTMLInputElement).value
+  const js = ($js as HTMLInputElement).value
+
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+        <style>${css}</style>
+      </head>
+      <body>
+        ${html}
+        <script>${js}</script>
+      </body>
+    </html>
+  `
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{html,js,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        primary: '#F3F4F6',
+        secondary: '#E5E7EB',
+        dark: '#1f1f1f',
+        danger: '#DC2626',
+        success: '#10B981',
+        warning: '#F59E0B',
+        info: '#3B82F6'
+      })
+    }
   },
   plugins: []
 }
